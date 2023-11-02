fbf1643b626b58d1b2ca83d561f10ec211dd5a82
inabaaaaa <nakbarsyarif@gmail.com>
2023-11-02 19:15:10 +0700

    get history done (need check first)

--- src/Routers/main.router.js
+++ src/Routers/main.router.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mainRouter = express.Router();
 
 const authRouter = require("./auth.router");
+const transactionRouter = require("./transaction.router");
 
 mainRouter.use("/auth", authRouter);
+mainRouter.use("/transaction", transactionRouter);
 
 module.exports = mainRouter;
