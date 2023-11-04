febf85e3acb41f5b426888f15d71ad0caf2778da
inabaaaaa <nakbarsyarif@gmail.com>
2023-11-04 16:31:53 +0700

    delete Transaction

--- src/Routers/transaction.router.js
+++ src/Routers/transaction.router.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const transactionRouter = express.Router();
-const { getHistory, transactionChart, getDashboardData } = require("../Handlers/transaction.handler");
+const { getHistory, transactionChart, getDashboardData, deleteTransaction } = require("../Handlers/transaction.handler");
 const { isLogin } = require("../Middlewares/authorization");
 
 transactionRouter.get("/:userid", isLogin, getHistory);
 transactionRouter.get("/chart/:userid", isLogin, transactionChart);
 transactionRouter.get("/dashboard/:userid", isLogin, getDashboardData);
+transactionRouter.delete("/:transactionId", isLogin, deleteTransaction);
 
 module.exports = transactionRouter;
