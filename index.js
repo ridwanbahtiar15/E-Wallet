4f08ed70694bc2fabf396faa11f01d1639e59b33
inabaaaaa <nakbarsyarif@gmail.com>
2023-11-06 17:36:34 +0700

    try fix cors

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 require("dotenv").config();
-const express = require('express')
+const express = require("express");
 const server = express();
 const mainRouter = require("./src/Routers/main.router");
 const cors = require("cors");
 
-
 server.listen(1600, () => {
-    console.log("Server is running at port 1600");
-  });
+  console.log("Server is running at port 1600");
+});
 server.use(express.json());
-server.use(express.urlencoded({extended: false}));
+server.use(express.urlencoded({ extended: false }));
 server.use(express.static("./public"));
 server.use(mainRouter);
 server.use(
-    cors({
-      origin: "http://localhost:5173/",
-      methods: ["PATCH","POST", "DELETE"],
-    })
-  );
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["PATCH", "POST", "DELETE"],
+  })
+);
 server.get("/", (req, res) => {
-    const data = "Hello World";
-    res.status(200).json({
-        msg: data
-    })
-})
+  const data = "Hello World";
+  res.status(200).json({
+    msg: data,
+  });
+});
